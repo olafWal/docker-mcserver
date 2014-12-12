b1a6996e3399b12403d90f437a02da16a491046c
Olaf Walkowiak <olaf@olaf-Lenovo-G770>
2014-12-12 16:09:01 +0100

    Better init

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM ubuntu:latest
 MAINTAINER Olaf Walkowiak <o.walkowiak@etent.de>
+VOLUME /var/data
 RUN apt-get update # Fri Oct 24 13:09:23 EDT 2014
 RUN apt-get -y upgrade
+RUN apt-get install psmisc
 ADD ./build/MCServer /var/MCServer
 ADD ./scripts /var/scripts
 RUN mkdir /var/log/supervisor/
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,49 @@
 #! /bin/bash
+DATADIR=/var/data
+WORLDDIR=${DATADIR}/worlds
+RUNDIR=/var/MCServer
+# First start code
+if [ ! -f ${DATADIR}/initialized ]; then
+    echo "Moving files to volume and symlinking"
+    for i in "banlist.sqlite" "motd.txt" "Ranks.sqlite" "settings.ini" "webadmin.ini" "ProtectionAreas.ini" "ProtectionAreas.sqlite"; do
+	echo  mv ${RUNDIR}/$i ${DATADIR}
+	mv ${RUNDIR}/$i ${DATADIR}
+	echo ln -s ${DATADIR}/$i ${RUNDIR}/$i
+        ln -s ${DATADIR}/$i ${RUNDIR}/$i
+    done
+
+    echo "Adding existing worlds to config"
+    WORLDSTR="DefaultWorld"
+    for i in $(ls ${WORLDDIR}); do
+	if [ -d ${WORLDDIR}/$i ]; then 
+	    echo LINK: ln -s ${WORLDDIR}/$i ${RUNDIR}
+	    ln -s ${WORLDDIR}/$i ${RUNDIR}
+	    echo $WORLDSTR=$i >> ${DATADIR}/settings.ini
+	    WORLDSTR="World"
+	fi
+    done
+
+    touch ${DATADIR}/initialized
+else
+    # It may be an update
+    for i in $(ls ${WORLDDIR}); do
+	if [ -d ${WORLDDIR}/$i ]; then 
+	    if [ -d ${RUNDIR}/$i ]; then
+		continue;
+	    fi
+	    echo UPDATE: ln -s ${WORLDDIR}/$i ${RUNDIR}
+	    ln -s ${WORLDDIR}/$i ${RUNDIR}
+	fi
+    done
+    echo "Symlinking files from datadir"
+    for i in "banlist.sqlite" "motd.txt" "Ranks.sqlite" "settings.ini" "webadmin.ini" "ProtectionAreas.ini" "ProtectionAreas.sqlite"; do
+	rm -f ${RUNDIR}/$i 
+        ln -s ${DATADIR}/$i ${RUNDIR}/$i
+    done
+fi;
+
+
+
+
 cd /var/MCServer
 ./MCServer
